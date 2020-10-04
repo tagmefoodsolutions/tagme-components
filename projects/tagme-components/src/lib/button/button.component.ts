a47f3efd41f1e9ca06abbed556e4921a2656bd3a
taymison <taymison@tagme.com.br>
2020-10-04 20:37:37 -0300

    Typography variables created

--- projects/tagme-components/src/lib/button/button.component.ts
+++ projects/tagme-components/src/lib/button/button.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'tg-button',
-	template: '<button>OK</button>'
+	template: '<button>OK</button>',
+	styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {}
