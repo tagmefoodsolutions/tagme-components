8903ee68399bf858f2b8df622fc9a732565bcf91
taymison <taymison@tagme.com.br>
2020-10-13 12:04:13 -0300

    Showcase removed

--- projects/tagme-components-showcase/src/app/pages/counters/counters.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-counters',
-	templateUrl: './counters.component.html',
-	styleUrls: ['../../app.component.scss']
-})
-export class CountersComponent {}
--- projects/tagme-components-showcase/src/app/pages/links/links.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'app-links',
-	templateUrl: './links.component.html',
-	styleUrls: ['../../app.component.scss']
-})
-export class LinksComponents {}
