95300a13caa5bedb9d97f113509553f0f814385d
taymison <taymison@tagme.com.br>
2020-10-20 14:01:27 -0300

    Comment added

--- projects/tagme-components/src/lib/country-badge/country-badge.component.ts
+++ projects/tagme-components/src/lib/country-badge/country-badge.component.ts
@@ -18,6 +18,64 @@ export class CountryBadgeComponent {
 
 	/**
 	 * Sets the name of the country
+	 * It can be:
+	 * 
+	 * - south-africa
+	 * - germany
+	 * - argentina
+	 * - australia
+	 * - austria
+	 * - belgium
+	 * - bolivia
+	 * - brazil
+	 * - bulgaria
+	 * - canada
+	 * - chile
+	 * - china
+	 * - colombia
+	 * - south-koreia
+	 * - croatia
+	 * - denmark
+	 * - emirates
+	 * - scotia
+	 * - slovenia
+	 * - spain
+	 * - spain
+	 * - finland
+	 * - france
+	 * - georgia
+	 * - greece
+	 * - netherlands
+	 * - hungary
+	 * - india
+	 * - indonesia
+	 * - england
+	 * - ireland
+	 * - israel
+	 * - italy
+	 * - jamaica
+	 * - japan
+	 * - lebanon
+	 * - malta
+	 * - marocco
+	 * - mexico
+	 * - norway
+	 * - new-zeland
+	 * - peru
+	 * - poland
+	 * - portugal
+	 * - czech-republic
+	 * - romania
+	 * - russia
+	 * - serbia-and-montenegro
+	 * - sweden
+	 * - switzerland
+	 * - tunisia
+	 * - turkey
+	 * - ukraine
+	 * - uruguay
+	 * - usa
+	 * - venezuela
 	 */
 	@Input() country: string;
 
